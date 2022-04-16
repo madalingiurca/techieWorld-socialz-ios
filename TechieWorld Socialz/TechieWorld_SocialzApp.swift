828d3cd321c59486411a8806ed382e442f2d17f4
madalingiurca <mgiurca.mail@gmail.com>
2022-04-17 00:51:06 +0300

    Initial Commit

---
+++ TechieWorld Socialz/TechieWorld_SocialzApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechieWorld_SocialzApp.swift
+//  TechieWorld Socialz
+//
+//  Created by Madalin Giurca on 17.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TechieWorld_SocialzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
